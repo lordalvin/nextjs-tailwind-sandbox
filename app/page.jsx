8688715f31b7708ae7bf663167749902a9c600e0
lordalvin <alvin041299@gmail.com>
2024-08-06 17:42:54 +0700

    yes

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Homepage = () => {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1 className="text-6xl">Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora voluptatum repellendus aspernatur iure maxime? Animi facere, modi molestias nihil atque, officia consequuntur debitis blanditiis dolorum ullam repellat iste? Numquam!
       </p>
