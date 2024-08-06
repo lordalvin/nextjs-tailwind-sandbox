d54d41428036726857ced806228b0e1da36627bb
lordalvin <alvin041299@gmail.com>
2024-08-06 17:37:04 +0700

    yes

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque tempora voluptatum repellendus aspernatur iure maxime? Animi facere, modi molestias nihil atque, officia consequuntur debitis blanditiis dolorum ullam repellat iste? Numquam!
+      </p>
+    </div>
+  )
+}
+
+export default Homepage
